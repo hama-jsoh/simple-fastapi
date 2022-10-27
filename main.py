c2911a863c93b63a0cf17b394dfbf71dc9be0fc9
hama-jsoh <111451710+hama-jsoh@users.noreply.github.com>
2022-10-27 21:10:12 +0900

    :zap: Add argparser args
    - host
    - port

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import uvicorn
+import argparse
 from fastapi import FastAPI
 from routes import test
 
+
+def set_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="Bind socket to this host. [default: 0.0.0.0]")
+    parser.add_argument("--port", type=int, default="8000", help="Bind socket to this host. [default: 80]")
+    args = parser.parse_args()
+    return args
 
 def create_app():
     app = FastAPI()
     app.include_router(test.router, tags=["test"], prefix="/test")
     return app
 
+
+ARGS = set_argument()
+HOST = ARGS.host
+PORT = ARGS.port
+
 app = create_app()
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8890, reload=True)
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
